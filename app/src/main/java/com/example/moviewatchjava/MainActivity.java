93c4c5234732b7a8993bd1dca276093a0b217843
Cade Johnson <crj30@pitt.edu>
2023-04-30 01:37:23 -0400

    submission

--- app/src/main/java/com/example/moviewatchjava/MainActivity.java
+++ app/src/main/java/com/example/moviewatchjava/MainActivity.java
@@ -5,32 +5,69 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
-
-    private ListView movieListView;
+public class MainActivity extends AppCompatActivity
+{
+    private ListView noteListView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initWidgets();
-        setNoteAdapter();
+        loadFromDBToMemory();
+        setMovieAdapter();
+        setOnClickListener();
     }
 
 
-    private void initWidgets() {
-        movieListView = findViewById(R.id.movieListView);
+    private void initWidgets()
+    {
+        noteListView = findViewById(R.id.noteListView);
     }
 
-    private void setNoteAdapter() {
-        NoteAdapter noteAdapter = new NoteAdapter(getApplicationContext(), Note.noteArrayList);
-        movieListView.setAdapter(noteAdapter);
+    private void loadFromDBToMemory()
+    {
+        SQLiteManager sqLiteManager = SQLiteManager.instanceOfDatabase(this);
+        sqLiteManager.populateNoteListArray();
     }
-    public void newMovie(View view) {
+
+    private void setMovieAdapter()
+    {
+        MovieAdapter movieAdapter = new MovieAdapter(getApplicationContext(), Note.nonDeletedNotes());
+        noteListView.setAdapter(movieAdapter);
+    }
+
+
+    private void setOnClickListener()
+    {
+        noteListView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l)
+            {
+                Note selectedNote = (Note) noteListView.getItemAtPosition(position);
+                Intent editNoteIntent = new Intent(getApplicationContext(), MovieDetailActivity.class);
+                editNoteIntent.putExtra(Note.NOTE_EDIT_EXTRA, selectedNote.getId());
+                startActivity(editNoteIntent);
+            }
+        });
+    }
+
+
+    public void newNote(View view)
+    {
         Intent newNoteIntent = new Intent(this, MovieDetailActivity.class);
         startActivity(newNoteIntent);
+    }
 
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        setMovieAdapter();
     }
 }
--- app/src/main/java/com/example/moviewatchjava/MovieDetailActivity.java
+++ app/src/main/java/com/example/moviewatchjava/MovieDetailActivity.java
@@ -2,34 +2,82 @@ package com.example.moviewatchjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.Button;
+import android.widget.EditText;
 
-public class MovieDetailActivity extends AppCompatActivity {
+import java.util.Date;
 
+public class MovieDetailActivity extends AppCompatActivity
+{
     private EditText titleEditText, descEditText;
+    private Button deleteButton;
+    private Note selectedNote;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail);
         initWidgets();
+        checkForEditNote();
     }
 
-    private void initWidgets(){
+    private void initWidgets()
+    {
         titleEditText = findViewById(R.id.titleEditText);
         descEditText = findViewById(R.id.descriptionEditText);
-
+        deleteButton = findViewById(R.id.deleteNoteButton);
     }
 
-    public void saveMovie(View view) {
-        String title = String.valueOf(titleEditText.getText());
-        String desc =  String.valueOf(descEditText.getText());
+    private void checkForEditNote()
+    {
+        Intent previousIntent = getIntent();
 
-        int id = Note.noteArrayList.size();
-        Note newNote = new Note(id, title, desc);
+        int passedNoteID = previousIntent.getIntExtra(Note.NOTE_EDIT_EXTRA, -1);
+        selectedNote = Note.getNoteForID(passedNoteID);
+
+        if (selectedNote != null)
+        {
+            titleEditText.setText(selectedNote.getTitle());
+            descEditText.setText(selectedNote.getDescription());
+        }
+        else
+        {
+            deleteButton.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    public void saveNote(View view)
+    {
+        SQLiteManager sqLiteManager = SQLiteManager.instanceOfDatabase(this);
+        String title = String.valueOf(titleEditText.getText());
+        String desc = String.valueOf(descEditText.getText());
+
+        if(selectedNote == null)
+        {
+            int id = Note.noteArrayList.size();
+            Note newNote = new Note(id, title, desc);
+            Note.noteArrayList.add(newNote);
+            sqLiteManager.addNoteToDatabase(newNote);
+        }
+        else
+        {
+            selectedNote.setTitle(title);
+            selectedNote.setDescription(desc);
+            sqLiteManager.updateNoteInDB(selectedNote);
+        }
+
+        finish();
+    }
+
+    public void deleteNote(View view)
+    {
+        selectedNote.setDeleted(new Date());
+        SQLiteManager sqLiteManager = SQLiteManager.instanceOfDatabase(this);
+        sqLiteManager.updateNoteInDB(selectedNote);
         finish();
     }
 }
